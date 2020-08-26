0099299439fa7c164e609c86dbef5c3d06304f9c
StibbardsJ <stibbardsjohn@gmail.com>
2020-08-25 22:52:33 -0600

    helloworld servlet functional

--- src/main/java/project/App.java
+++ src/main/java/project/App.java
@@ -1,13 +1,18 @@
 package project;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.File;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+public class App {
+    public static void main( String[] args ) throws LifecycleException {
+        Tomcat server = new Tomcat();
+        server.setBaseDir(new File("project/target/tomcat/").getAbsolutePath());
+        server.setPort(8443);
+        server.getConnector();
+        server.addWebapp("/embed", new File("project/src/main/resources/").getAbsolutePath());
+        server.addServlet("/embed", "HelloServlet", new HelloServlet()).addMapping("/hello");
+        server.start();
     }
 }
